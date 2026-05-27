67aa706d67f9cde299f2ee5aba4f8e4dc9180ca1
Rodrigo Artur <rudrugoa@gmail.com>
2026-05-27 17:22:55 -0300

    Atualiza paleta visual estilo GymControl

--- app/src/main/java/com/example/projetoacademia/ui/theme/Color.kt
+++ app/src/main/java/com/example/projetoacademia/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.example.projetoacademia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GymBackground = Color(0xFF061017)
+val GymSurface = Color(0xFF0E1A22)
+val GymSurfaceVariant = Color(0xFF14232E)
+val GymPrimary = Color(0xFF57D323)
+val GymPrimaryDark = Color(0xFF1B8F18)
+val GymSecondary = Color(0xFF0B84FF)
+val GymText = Color(0xFFF4F8FB)
+val GymTextMuted = Color(0xFF9EADB8)
+val GymError = Color(0xFFFF5C5C)
+val GymWarning = Color(0xFFFFC247)
